187c1389f1665e111109073e42d7ca442ada6329
Cherry Chung <bubibuuu@gmail.com>
2020-06-02 14:49:46 +0800

    新增HomeController

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,16 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+// ==============================================
+Route::get('/users/{user}', function(\App\User $user){
+    return $user;
+});
+
+// Voyager套件規則 ==============================
+Route::group(['prefix' => 'admin'], function () {
+    Voyager::routes();
+});
+
+Auth::routes();
+Route::get('/home', 'HomeController@index')->name('home');
