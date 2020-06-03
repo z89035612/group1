bd4043181fef2f6339343ec82f3a3bbb58188c11
Cherry Chung <bubibuuu@gmail.com>
2020-06-03 14:03:05 +0800

    登入頁面本地化

---
+++ resources/lang/zh_TW/generic.php
@@ -0,0 +1,14 @@
+<?php
+
+return[
+    "welcome" => "歡迎使用我們的網頁服務",
+    "email" => "電子郵件",
+    "name" => "使用者名稱",
+    "username" => "帳號",
+    "password" => "密碼",
+    "login" => "登入",
+    "register" => "註冊",
+    "remember" => "記住密碼",
+    "forget_password" => "忘記密碼",
+    "confirm_password" => "密碼確認"
+];
